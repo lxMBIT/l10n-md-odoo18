c15d886fabccc9deeac2482bfc23c8aff067d96d
OdooBot <odoo-bot@lxmbit.md>
2025-07-03 03:00:14 +0000

    feat(ui): подключены автосгенерированные views для всех моделей, обеспечена полная доступность через интерфейс

--- l10n_md_einvoice/__manifest__.py
+++ l10n_md_einvoice/__manifest__.py
@@ -7,6 +7,12 @@
     'website': 'https://github.com/lxMBIT/l10n-md-odoo18',
     'depends': ['base', 'account'],
     'data': [
+        'views/api_autogen_views.xml',
+        'views/xml_template_autogen_views.xml',
+        'views/invoice_autogen_views.xml',
+        'views/soap_autogen_views.xml',
+        'views/einvoice_send_wizard_view.xml',
+        'views/account_move_button_einvoice.xml',
         'demo/einvoice_invoice_demo.xml',
     ],
     'demo': [
